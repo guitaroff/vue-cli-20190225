3bb9f87f1985b10d8e1566296b7a3024dfdd4a9d
andrey <ag82pishi@yandex.ru>
2019-03-13 13:47:08 +0300

    adds 'EditUser' page(in progress)

---
+++ src/components/UserForm.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <form>
+      <div class="form-group">
+        <label for="firstName">Имя</label>
+        <input
+          id="firstName"
+          type="text"
+          :value="user.firstName"
+          class="form-control"
+          placeholder="Введите имя"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="lastName">Фамилия</label>
+        <input
+          id="lastName"
+          type="text"
+          :value="user.lastName"
+          class="form-control"
+          placeholder="Введите фамилию"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="age">Возраст</label>
+        <input id="age" type="text" :value="user.age" class="form-control" placeholder="Возраст" />
+      </div>
+
+      <div class="form-group">
+        <label for="phone">Телефон</label>
+        <input
+          id="phone"
+          type="text"
+          :value="user.phone"
+          class="form-control"
+          required
+          placeholder="Телефон"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="email">Эл.почта</label>
+        <input
+          id="email"
+          type="text"
+          :value="user.email"
+          class="form-control"
+          required
+          placeholder="Эл.почта"
+        />
+      </div>
+
+      <button type="submit" class="btn btn-primary" @click.prevent="updateUser">Обновить</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserForm',
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    updateUser() {
+      console.log('user', this.user)
+      this.$emit('user', this.user)
+    }
+  }
+}
+</script>
--- src/views/EditUser.vue
+++ src/views/EditUser.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-    EDIT USER
+    <user-form :user="user"></user-form>
+    <pre>{{ user }}</pre>
   </div>
 </template>
 
 <script>
+import UserForm from '@/components/UserForm.vue'
+import axios from 'axios'
+
 export default {
-  name: 'EditUser'
+  name: 'EditUser',
+  components: {
+    UserForm
+  },
+  data() {
+    return {
+      user: {}
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  },
+  mounted() {
+    this.loadUser()
+  },
+  methods: {
+    loadUser() {
+      axios
+        .get('http://localhost:3004/users/' + this.id)
+        .then(response => response.data)
+        .then(user => {
+          this.user = user
+        })
+        .catch(error => {
+          if (error.response) {
+            console.log('error response', error.response)
+          } else if (error.request) {
+            console.log('error request', error.request)
+          } else {
+            console.log('Error message', error.message)
+          }
+        })
+    }
+  }
 }
 </script>
